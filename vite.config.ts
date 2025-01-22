cebc6f9aee2930eb84a3694e281e060ab32ce418
bienoubien-studio <salut@bienoubien.studio>
2025-01-22 07:19:02 +0100

    Add adapter-node & update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [rizom(), sveltekit()],
+
 	server: {
 		host: 'rizom.test'
 	},
-	build: {
-		rollupOptions: {
-			external: ['@node-rs/argon2']
-		}
-	},
+
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
