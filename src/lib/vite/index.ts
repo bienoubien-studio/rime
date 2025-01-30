638c41243dced123db2d80928adde89e924847a5
bienoubien-studio <salut@bienoubien.studio>
2025-01-30 09:46:36 +0100

    Clean

--- src/lib/vite/index.ts
+++ src/lib/vite/index.ts
@@ -6,17 +6,7 @@ dotenv.config({ override: true });
 export function rizom(): Plugin {
 	return {
 		name: 'rizom',
-		async handleHotUpdate({ file, server }) {
-			if (file.includes('src/config')) {
-				server.ws.send({
-					type: 'custom',
-					event: 'special-update',
-					data: {}
-				});
-			}
-			server.ws.send({ type: 'full-reload' });
-			return [];
-		},
+
 		config(): UserConfig {
 			return {
 				build: {
