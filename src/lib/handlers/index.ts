6b8f7cc1b89eadefe549b106416266649db2d752
bienoubien-studio <salut@bienoubien.studio>
2025-02-18 14:21:41 +0100

    Remove CORS handler for now

--- src/lib/handlers/index.ts
+++ src/lib/handlers/index.ts
@@ -1,6 +1,6 @@
 import { handleAuth } from './auth.server.js';
 import { createCMSHandler } from './rizom.server.js';
-import { handleCORS } from './cors.server.js';
+// import { handleCORS } from './cors.server.js';
 import { handleRoutes } from './routes.server.js';
 import type { Config } from 'rizom/types/index.js';
 import { createPluginsHandler } from './plugins.server.js';
@@ -10,7 +10,7 @@ export default function (args: Args) {
 	return [
 		createCMSHandler(args),
 		handleAuth,
-		handleCORS,
+		// handleCORS,
 		handleRoutes,
 		...createPluginsHandler({ config: args.config })
 	];
