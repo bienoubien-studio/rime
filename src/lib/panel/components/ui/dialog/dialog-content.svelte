782ff236c0a931d3b34cc07841772476cae73e05
bienoubien-studio <salut@bienoubien.studio>
2025-03-08 14:17:01 +0100

    Global dialog-content css to file

--- src/lib/panel/components/ui/dialog/dialog-content.svelte
+++ src/lib/panel/components/ui/dialog/dialog-content.svelte
@@ -3,6 +3,7 @@
 	import X from 'lucide-svelte/icons/x';
 	import type { Snippet } from 'svelte';
 	import * as Dialog from './index.js';
+	import './dialog-content.css';
 
 	let {
 		ref = $bindable(null),
@@ -24,55 +25,3 @@
 		</DialogPrimitive.Close>
 	</DialogPrimitive.Content>
 </Dialog.Portal>
-
-<style type="postcss">
-	:global {
-		.rz-dialog-content {
-			@mixin bg color-bg;
-			position: fixed;
-			left: 50%;
-			top: 40%;
-			z-index: 50;
-			display: grid;
-			width: 100%;
-			max-width: 32rem;
-			transform: translate(-50%, -50%);
-			gap: var(--rz-size-4);
-			border: var(--rz-border);
-			padding: var(--rz-size-6);
-			box-shadow: var(--rz-shadow-lg);
-		}
-
-		@media (min-width: 640px) {
-			.rz-dialog-content {
-				border-radius: var(--rz-radius-lg);
-			}
-		}
-
-		@media (min-width: 768px) {
-			.rz-dialog-content {
-				width: 100%;
-			}
-		}
-
-		.rz-dialog-content__close {
-			@mixin color color-fg, 0.7;
-			position: absolute;
-			right: var(--rz-size-4);
-			top: var(--rz-size-3);
-			border-radius: var(--rz-radius-sm);
-			opacity: 0.7;
-			transition: opacity 0.2s;
-		}
-
-		.rz-dialog-content__close:focus {
-			outline: none;
-			/* --rz-ring-offset-2: 2px; */
-			@mixin ring var(--rz-color-ring);
-		}
-
-		.rz-dialog-content__close:disabled {
-			pointer-events: none;
-		}
-	}
-</style>
