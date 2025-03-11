b0d6a24ea057a1cf583346906cba10d734022a36
bienoubien-studio <salut@bienoubien.studio>
2025-03-11 12:10:17 +0100

    Fix types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,6 @@
 import rizom from './rizom.server.js';
 import handlers from './handlers/index.js';
 import { buildConfig } from './config/build/index.js';
-
 export { rizom, handlers, buildConfig };
 export { FormFieldBuilder } from 'rizom/fields/builders/field.js';
 export { area, collection } from 'rizom/config/builders.js';
@@ -28,24 +27,18 @@ export type {
 export type { Link } from 'rizom/fields/link/index.js';
 export type { UploadDoc, BaseDoc } from './types/doc.js';
 export type { User } from 'rizom/types/auth.js';
-export type { Plugin } from 'rizom/types/plugin.js';
+export type { Plugin, Plugins } from 'rizom/plugins/index.js';
 export type { Rizom } from 'rizom/rizom.server.js';
 export type { LocalAPI } from 'rizom/types/api.js';
 export type { Navigation } from './panel/navigation.js';
 export type { FormField, AnyField } from 'rizom/types';
 
 declare module 'rizom' {
-	// eslint-disable-next-line
-	interface RegisterFieldsType {}
-	// eslint-disable-next-line
-	interface RegisterFormFields {}
-	// eslint-disable-next-line
-	interface RegisterFields {}
-	// eslint-disable-next-line
-	interface RegisterPlugins {}
-	// eslint-disable-next-line
+	export interface RegisterFieldsType {}
+	export interface RegisterFormFields {}
+	export interface RegisterFields {}
+	export interface RegisterPlugins {}
 	export interface RegisterCollection {}
-	// eslint-disable-next-line
 	export interface RegisterArea {}
 
 	export interface Register {
