86c76b63567b5fa2ead80f344d5533cd1078f710
bienoubien-studio <salut@bienoubien.studio>
2025-03-12 11:53:53 +0100

    Fix - missing export

--- src/lib/panel/index.server.ts
+++ src/lib/panel/index.server.ts
@@ -1,3 +1,4 @@
 import { checkLiveRedirect } from './util/live.server.js';
+import { registerTranslation } from './i18n/register.server.js';
 
-export { checkLiveRedirect };
+export { checkLiveRedirect, registerTranslation };
