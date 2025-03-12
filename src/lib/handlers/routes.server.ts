9f5cbc6568c005c3c65eb45cdf2dbc75165e0379
bienoubien-studio <salut@bienoubien.studio>
2025-03-12 11:55:04 +0100

    Clean

--- src/lib/handlers/routes.server.ts
+++ src/lib/handlers/routes.server.ts
@@ -2,23 +2,12 @@ import { json, type Handle } from '@sveltejs/kit';
 import apiInit from '../api/init.js';
 import { logout } from 'rizom/panel/pages/logout/actions.server.js';
 import buildNavigation from '$lib/panel/navigation.js';
-import { PACKAGE_NAME } from 'rizom/constant.js';
-import { dev } from '$app/environment';
-import { existsSync } from 'fs';
-import path from 'path';
-import cache from 'rizom/bin/generate/cache/index.js';
-
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const handleRoutes: Handle = async ({ event, resolve }) => {
 	const { rizom, user } = event.locals;
 
 	if (event.url.pathname === '/api/reload-config') {
 		return json({ success: true });
-	}
-
-	if (event.url.pathname?.startsWith('/i18n')) {
-		return await handleTranslations(event.url.pathname);
 	}
 
 	if (event.url.pathname?.startsWith('/panel') && event.request.method === 'GET') {
@@ -42,27 +31,4 @@ export const handleRoutes: Handle = async ({ event, resolve }) => {
 	}
 
 	return resolve(event);
-};
-
-const handleTranslations = async (pathname: string) => {
-	// Extract locale and namespace from path
-	const [_, _i18n, locale, namespace] = pathname.split('/');
-
-	try {
-		// Import the translations
-		const isPackageDev = process.env.RIZOM_ENV === 'package';
-		const pathToTranslation = isPackageDev
-			? `../panel/i18n/${locale}/${namespace}.js`
-			: `${PACKAGE_NAME}/panel/i18n/${locale}/${namespace}.js`;
-		const translations = await import(/* @vite-ignore */ pathToTranslation);
-
-		return new Response(JSON.stringify(translations.default), {
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		});
-	} catch (error: any) {
-		console.error('Failed to load translations:', error.message);
-		return new Response('Translation not found', { status: 404 });
-	}
 };
