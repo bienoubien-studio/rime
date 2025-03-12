cd9f9a9793015fd6cd50a0c51dc8d67942865b1d
bienoubien-studio <salut@bienoubien.studio>
2025-03-12 12:33:40 +0100

    Fix - betterAuth types

--- src/lib/panel/util/auth.ts
+++ src/lib/panel/util/auth.ts
@@ -1,6 +1,6 @@
 import { env } from '$env/dynamic/public';
 import { createAuthClient } from 'better-auth/svelte';
 
-export const authClient = createAuthClient({
+export const authClient: ReturnType<typeof createAuthClient> = createAuthClient({
 	baseURL: env.PUBLIC_RIZOM_URL
 });
