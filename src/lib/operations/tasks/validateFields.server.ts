0cd81a7a0188792098c5de47f0f22a357626eae9
bienoubien-studio <salut@bienoubien.studio>
2025-03-12 11:55:45 +0100

    Fix - unique fields validation not working for update AND create

--- src/lib/operations/tasks/validateFields.server.ts
+++ src/lib/operations/tasks/validateFields.server.ts
@@ -22,8 +22,8 @@ export const validateFields = async <T extends GenericDoc>(args: {
 }) => {
 	const errors: FormErrors = {};
 	const { api, locale, configMap, original, operation, user } = args;
-	const { adapter } = api.rizom;
-	const isCollection = api.rizom.config.getDocumentPrototype(args.config.slug);
+	const slug = args.config.slug;
+	const isCollection = api.rizom.config.isCollection(slug);
 	let output = { ...args.data };
 
 	for (const [key, config] of Object.entries(configMap)) {
@@ -60,10 +60,12 @@ export const validateFields = async <T extends GenericDoc>(args: {
 		 *   and parse the sqlite error ??
 		 */
 		if ('unique' in config && config.unique && isCollection) {
-			const query = { where: { [key]: { equals: value } } };
-			// const query = `where[${key}][equals]=${field.value}`;
-			const existing = await adapter.collection.query({ slug: args.config.slug, query });
-			if (original?.id && existing.length && existing[0].id !== original?.id) {
+			const query =
+				operation === 'create'
+					? `where[${key}][equals]=${value}`
+					: `where[and][0][${key}][equals]=${value}&where[and][1][id][not_equals]=${original?.id}`;
+			const existing = await api.collection(slug).find({ locale, query });
+			if (existing.length) {
 				errors[key] = RizomFormError.UNIQUE_FIELD;
 			}
 		}
