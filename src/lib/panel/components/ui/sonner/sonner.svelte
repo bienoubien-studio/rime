712fb6c88d95240cd02b490335a46932bf34a1f7
bienoubien-studio <salut@bienoubien.studio>
2025-04-29 11:59:53 +0200

    Remove mode-watcher dependency

--- src/lib/panel/components/ui/sonner/sonner.svelte
+++ src/lib/panel/components/ui/sonner/sonner.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
 	import { Toaster as Sonner, type ToasterProps as SonnerProps } from 'svelte-sonner';
-	import { mode } from 'mode-watcher';
 	import './sonner.css';
 
 	let restProps: SonnerProps = $props();
 </script>
 
 <Sonner
-	theme={$mode}
 	class="rz-toaster"
 	toastOptions={{
 		classes: {
