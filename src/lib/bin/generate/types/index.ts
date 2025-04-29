6d4a325ba97eb00f6bdce40b6bf3c17c8c08dcf9
bienoubien-studio <salut@bienoubien.studio>
2025-04-29 08:26:23 +0200

    Remove _children type on nested collection doc as children aren't autopopulated

--- src/lib/bin/generate/types/index.ts
+++ src/lib/bin/generate/types/index.ts
@@ -132,9 +132,6 @@ export type RelationValue<T> =
 					fieldsContent += generateImageSizesType(collection.imageSizes);
 				}
 			}
-			if(collection.nested){
-				fieldsContent += `\n\t_children: RelationValue<${makeDocTypeName(collection.slug)}>[]`
-			}
 			return templateDocType(collection.slug, fieldsContent, collection.upload);
 		})
 		.join('\n');
