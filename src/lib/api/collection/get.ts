2d6857b4198280df17bd92fc75efd402159247ec
bienoubien-studio <salut@bienoubien.studio>
2025-05-07 07:38:17 +0200

    Missing API handling

--- src/lib/api/collection/get.ts
+++ src/lib/api/collection/get.ts
@@ -3,6 +3,7 @@ import { json, type RequestEvent } from '@sveltejs/kit';
 import { handleError } from '$lib/errors/handler.server.js';
 import type { CollectionSlug } from '$lib/types/doc.js';
 import { safe } from '$lib/util/safe.js';
+import type { Dic } from 'rizom/types/util';
 
 
 export default function (slug: CollectionSlug) {
@@ -12,28 +13,28 @@ export default function (slug: CollectionSlug) {
 		const params = event.url.searchParams;
 
 		const hasQueryParams = !!params.keys().filter(key => key.startsWith('where')).toArray().length;
+		const hasSelectParams = !!params.keys().filter(key => key === 'select').toArray().length;
 
-		let apiParams;
-		let apiMethod: 'find' | 'findAll';
+		let apiParams:Dic = {
+			locale: params.get('locale') || locale,
+			sort: params.get('sort') || '-createdAt',
+			depth: params.get('depth') ? parseInt(params.get('depth')!) : 0,
+			limit: params.get('limit') ? parseInt(params.get('limit')!) : undefined,
+			offset: params.get('offset') ? parseInt(params.get('offset')!) : undefined
+		};
 
-		if (hasQueryParams) {
-			apiParams = {
-				locale: params.get('locale') || locale,
-				sort: params.get('sort') || '-createdAt',
-				depth: params.get('depth') ? parseInt(params.get('depth')!) : 0,
-				limit: params.get('limit') ? parseInt(params.get('limit')!) : undefined,
-				offset: params.get('offset') ? parseInt(params.get('offset')!) : undefined,
-				query: qs.parse(event.url.search.substring(1))
-			};
+		let apiMethod: 'find' | 'findAll' | 'select';
+		
+		if( hasSelectParams) {
+			apiMethod = 'select';
+			apiParams.select = params.get('select')?.split(',') || []
+			if( hasQueryParams ){
+				apiParams.query = qs.parse(event.url.search.substring(1))
+			}
+		} else if (hasQueryParams) {
+			apiParams.query = qs.parse(event.url.search.substring(1))
 			apiMethod = 'find';
 		} else {
-			apiParams = {
-				locale: params.get('locale') || locale,
-				sort: params.get('sort') || '-createdAt',
-				depth: params.get('depth') ? parseInt(params.get('depth')!) : 0,
-				limit: params.get('limit') ? parseInt(params.get('limit')!) : undefined,
-				offset: params.get('offset') ? parseInt(params.get('offset')!) : undefined,
-			};
 			apiMethod = 'findAll';
 		}
 		
