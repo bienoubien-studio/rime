308d61905b01787264b7bb466072288d72164664
bienoubien-studio <salut@bienoubien.studio>
2025-08-06 16:00:09 +0200

    fix (panel) : prevent error when a block table has been remove from DB, resulting in an array of block with possible undefined item

--- src/lib/core/operations/hooks/before-read/process-document-fields.server.ts
+++ src/lib/core/operations/hooks/before-read/process-document-fields.server.ts
@@ -1,3 +1,4 @@
+import type { GenericBlock } from '$lib/core/types/doc.js';
 import { deleteValueAtPath, getValueAtPath, setValueAtPath } from '$lib/util/object.js';
 import { buildConfigMap } from '../../configMap/index.server.js';
 import { Hooks } from '../index.js';
@@ -7,8 +8,24 @@ export const processDocumentFields = Hooks.beforeRead( async (args) => {
 	let doc = args.doc;
   
 	const configMap = buildConfigMap(doc, args.config.fields);
-
+	
 	for (const [key, config] of Object.entries(configMap)) {
+
+		// Filter out possible undefined block
+		// Case : When in dev mode, if a block table is deleted in a migration, a blocks array value could includes undefined ex : 
+		// blocks: [ { ... }, undefined, { ... } ]
+		// Because blocks are populated based on path.position
+		if(config.type === 'blocks'){
+			const value = getValueAtPath<(GenericBlock | undefined)[]>(key, doc);
+			if( value && value.includes(undefined) ){
+				const withoutFalsyBlock = value.filter( b => Boolean(b)).map( (b, index) => ({
+					...b,
+					position: index
+				}))
+				doc = setValueAtPath(key, doc, withoutFalsyBlock)
+			}
+		}
+
 		if (config.access && config.access.read) {
 			const authorized = config.access.read(event.locals.user);
 			if (!authorized) {
