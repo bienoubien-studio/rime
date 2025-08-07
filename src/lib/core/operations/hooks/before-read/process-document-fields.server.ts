bf0042befb1616f1f3f1d627f9034a0327e4c6db
bienoubien-studio <salut@bienoubien.studio>
2025-08-07 10:19:37 +0200

    change (operation) : refactoring withoutFalsyBlock → withoutResidualBlock

--- src/lib/core/operations/hooks/before-read/process-document-fields.server.ts
+++ src/lib/core/operations/hooks/before-read/process-document-fields.server.ts
@@ -17,17 +17,17 @@ export const processDocumentFields = Hooks.beforeRead( async (args) => {
 			// Case undefined : When in dev mode, if a block table is deleted in a migration, a blocks array value could includes undefined ex : 
 			// * blocks: [ { ... }, undefined, { ... } ]
 			// * Because blocks are populated based on path.position
-			// Case residual : a block type has been removed including relation, ex :
+			// Case residual : a block type has been removed but was including a relation, this gives ex :
 			// * blocks: [ { id:..., values,... }, { image: {...}} ] 
 			// * the image has been placed but the block doesn't exist anymore
 			if( value ){
-				const withoutFalsyBlock = value
+				const withoutResidualBlock = value
 					.filter( b => b && 'id' in b) // get only blocks that have .id
 					.map( (b, index) => ({
 					...b,
 					position: index
 				}))
-				doc = setValueAtPath(key, doc, withoutFalsyBlock)
+				doc = setValueAtPath(key, doc, withoutResidualBlock)
 			}
 		}
 
